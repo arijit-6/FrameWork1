f8014c6cbc23cb50f55ababd9ca43bed97e811ba
Sam Smith <dfc@g.com>
2020-07-13 04:42:40 +0530

    commiting for branch1 second commit

--- src/main/java/BaseClass/LaunchBrowser.java
+++ src/main/java/BaseClass/LaunchBrowser.java
@@ -1,6 +1,51 @@
 package BaseClass;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LaunchBrowser {
+	
+	public static WebDriver driver;
+	
+	public static Properties prop;
+	
+	public static FileInputStream fls;
+	
+	public static void main(String [] args) throws IOException
+	{
+		
+		
+	//	System.setProperty("webdriver.chrome.driver","C://Program Files//chromedriver.exe");
+		
+		
+          prop = new Properties();	
+          
+          fls = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\config.properties");
+          
+          prop.load(fls);
+          
+          System.out.println(prop.getProperty("url"));
+		
+	/*	  System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") +"\\src\\main\\java\\Drivers\\chromedriver.exe");
+		
+		  driver = new ChromeDriver();
+		  
+		  driver.get("https://www.google.com");
+		  
+		  
+		  
+		 */ 
+		  
+		  
+		
+		 
+		
+	}
 	
 	
 

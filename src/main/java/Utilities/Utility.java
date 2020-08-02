22b4ea566eff8a019b1b29df2fc45efaac7a98de
Sam Smith <dfc@g.com>
2020-08-02 14:28:00 +0530

    committed

--- src/main/java/Utilities/Utility.java
+++ src/main/java/Utilities/Utility.java
@@ -1,7 +1,10 @@
 package Utilities;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Utility {
 	
@@ -29,5 +32,36 @@ public class Utility {
 		driver.findElement(element).click();
 		
 	}
+	
+	
+	
+	public void selectDate(By by, String textMonth, By by1, String textDate)
+	{
+		
+		
+		while(!driver.findElement(by).equals(textMonth))
+		{
+			driver.findElement(by1).click();
+			
+		}
+		
+		List<WebElement> dates = driver.findElements(by1);
+		
+		for(int i=0;i<dates.size();i++)
+		{
+			
+			if(dates.get(i).getText().equals(textDate))
+			{
+				dates.get(i).click();
+				
+				break;
+			}
+			
+		}
+		
+		
+	}
+	
+	
 
 }
